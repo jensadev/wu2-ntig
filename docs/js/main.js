852fdc75fb5208466ee8559d751154bdd3c2c506
jensnti <jens.andreasson@ga.ntig.se>
2019-08-30 11:10:51 +0200

    path changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,6 +3,6 @@ window.onload = () => {
   
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-               .register('/js/sw.js');
+               .register('js/sw.js');
     }
   }
